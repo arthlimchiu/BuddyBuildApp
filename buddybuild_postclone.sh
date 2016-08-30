06d24eac2f0442e3fc4b1702281546cb0790ac58
Arth Limchiu <arth@sym.ph>
2016-08-30 20:37:06 +0800

    asdasda

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -1,24 +1,20 @@
 #!/usr/bin/env bash
 
 #
-sed -i.original '/android {/i\
-def debugsuffix = System.getProperty("debugsuffix", project.getProperties().get("debugsuffix", null))\
-def final appId = "com.arthlimchiu.buddybuildapp"\
-' app/build.gradle
+# sed -i.original '/android {/i\
+# def debugsuffix = System.getProperty("debugsuffix", project.getProperties().get("debugsuffix", null))\
+# def final appId = "com.arthlimchiu.buddybuildapp"\
+# ' app/build.gradle
 
 #
-sed -i.original 's#applicationId "com.arthlimchiu.buddybuildapp"#applicationId = appId#g' app/build.gradle
+# sed -i.original 's#applicationId "com.arthlimchiu.buddybuildapp"#applicationId = appId#g' app/build.gradle
 
 #
-sed -i.original 's#applicationIdSuffix ".debug"#applicationIdSuffix ".$debugsuffix"#g' app/build.gradle
+# sed -i.original 's#applicationIdSuffix ".debug"#applicationIdSuffix ".$debugsuffix"#g' app/build.gradle
 
 #
 sed -i.original '/applicationIdSuffix/ a\
-resValue "string", "app_name", "BuddyBuildApp-$debugsuffix"\
+resValue "string", "app_name", "BuddyBuildApp-sampleapp"\
 ' app/build.gradle
 
 sed -i '/<string name="app_name">/d' app/src/main/res/values/strings.xml
-
-chmod +x gradlew
-
-./gradlew -Pdebugsuffix=bug50 assembleDebug installDebug
