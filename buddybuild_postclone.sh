098bc87767e4f9dd9a11d9a1687279ff77d2b15b
Arth Limchiu <arth@sym.ph>
2016-08-30 18:12:55 +0800

    Added postclone shell script

---
+++ buddybuild_postclone.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+#
+sed -i.original '/android {/i\
+def debugsuffix = System.getProperty("debugsuffix", project.getProperties().get("debugsuffix", "pokemon"))\
+def final appId = "com.arthlimchiu.buddybuildapp"\
+' app/build.gradle
+
+#
+sed -i.original 's#applicationId "com.arthlimchiu.buddybuildapp"#applicationId = appId#g' app/build.gradle
+
+#
+sed -i.original 's#applicationIdSuffix ".debug"#applicationIdSuffix ".$debugsuffix"#g' app/build.gradle
+
+#
+sed -i.original '/applicationIdSuffix/ a\
+resValue "string", "app_name", "JenkinsCI-$debugsuffix"\
+' app/build.gradle
+
+sed -i.original '/<string="app_name">/d' app/src/main/res/values/strings.xml
